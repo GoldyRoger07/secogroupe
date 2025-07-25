1cbb326057b43ab5a6308b99d41bc5e12d424102
Latouche Gerson <latouche822@gmail.com>
2025-07-25 15:54:23 -0400

    up-seco-1

--- studio/v3/assets/js/bd.js
+++ studio/v3/assets/js/bd.js
@@ -22,69 +22,75 @@ SERVICES["seco_tech"] = {
 SERVICES["seco_distributors"] = {
     title: "Seco Distributors",
     description: `
-     <p>
+     <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
     </p>
     `,
-    cover: "../images/new/bg_seco_distributor.jpg"
+    // cover: "../images/new/bg_seco_distributor.jpg"
+    cover: "https://www.plugandtrack.com/wp-content/uploads/2022/05/Normes-stockage-distribution-medicaments-e1652099964737.jpg"
 }
 
 SERVICES["seco_agro_industries"] = {
     title: "Seco Agro-Industries",
     description: `
-     <p>
+     <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
     </p>
     `,
-    cover: "../images/new/bg_seco_agro_industries.jpg"
+    // cover: "../images/new/bg_seco_agro_industries.jpg"
+    cover: "https://plus.unsplash.com/premium_photo-1661962573121-d4c317caf908?q=80&w=1529&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 }
 
 SERVICES["seco_energy_co"] = {
     title: "Seco Energy CO",
     description: `
-     <p>
+     <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
     </p>
-    `,cover: "../images/new/bg_seco_energy_co.jpg"
+    `//,cover: "../images/new/bg_seco_energy_co.jpg"
+    ,cover: "https://images.unsplash.com/photo-1473341304170-971dccb5ac1e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 }
 
 SERVICES["seco_univers_construction"] = {
     title: "Seco Univers Constructions",
-    description: ` <p>
+    description: ` <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
-    </p>`,cover: "../images/new/bg_seco_univers_construction.jpg"
+    </p>`,cover: "https://plus.unsplash.com/premium_photo-1661962573121-d4c317caf908?q=80&w=1529&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 }
 
+// ../images/new/bg_seco_univers_construction.jpg
+
 SERVICES["mass_assurances"] = {
     title: "Mass Assurances",
-    description:` <p>
+    description:` <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
     </p>`,
      cover: "../images/new/bg_mass_assurance.jpg"
+    // cover: "/home/sonix/Téléchargements/krakenimages-Y5bvRlcCx8k-unsplash.jpg"
 
 }
 
 SERVICES["mass_funds"] = {
     title: "Mass Funds",
-    description:` <p>
+    description:` <p style="color: #000;">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aperiam eius tempora, necessitatibus quod, inventore soluta voluptate rerum obcaecati esse corrupti! Voluptatum magni quis et magnam atque optio, quos esse!
--- studio/v3/assets/js/service.js
+++ studio/v3/assets/js/service.js
@@ -4,25 +4,30 @@ const serviceName = params.get("service_name")
 
 const serviceContainer = document.querySelector("#service-container")
 
-const titleService = document.querySelector("#title-service")
+const serviceTitle = document.querySelector("#service-title")
 
 const titleServiceActive = document.querySelector("#title-service-active")
 
 const innerBanner = document.querySelector(".inner-banner")
 
+if(!SERVICES[serviceName]){
+    let a = document.createElement("a")
+    a.href="index.html"
+    a.click();
+}
 
-
-console.log(SERVICES[serviceName])
 
 // innerBanner.style=`background: url(${SERVICES[serviceName].cover}) no-repeat top/cover; min-height: 100vh; position: fixed; left: 0; right: 0; bottom: 0; z-index:1;`
 // Modifier la variable globale --main-color
 document.documentElement.style.setProperty('--body-bg', `url(${SERVICES[serviceName].cover})`);
 
-serviceContainer.innerHTML = `
-                    <h3 class="title-style mb-2">${SERVICES[serviceName].title}</h3>
-                    <p class="mb-2">${SERVICES[serviceName].description}</p>
+// serviceContainer.innerHTML = `
+//                     <h3 class="title-style text-dark mb-2">${SERVICES[serviceName].title}</h3>
+//                     <p class="mb-2" style="color: #000;">${SERVICES[serviceName].description}</p>
                     
-`
+// `
 
-titleService.textContent = SERVICES[serviceName].title
-titleServiceActive.textContent = SERVICES[serviceName].title
+
+
+serviceTitle.textContent = SERVICES[serviceName].title
+// titleServiceActive.textContent = SERVICES[serviceName].title
